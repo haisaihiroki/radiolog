07ea0fd0daef9b0e057f5d8f5e22245816997cc4
Hiroki Chinen <haisaihiroki@gmail.com>
2019-01-23 22:50:44 +0900

    Add relations.

--- app/CommunicationLog.php
+++ app/CommunicationLog.php
@@ -6,5 +6,43 @@ use Illuminate\Database\Eloquent\Model;
 
 class CommunicationLog extends Model
 {
-    //
+    public  function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function modes()
+    {
+        return $this->hasOne('App\Mode');
+    }
+
+    public function my_Readability()
+    {
+        return $this->hasOne('App\Readability', 'id', 'my_r');
+    }
+
+    public function his_Readability()
+    {
+        return $this->hasOne('App\Readability', 'id', 'his_r');
+    }
+
+    public function my_SignalStrength()
+    {
+        return $this->hasOne('App\SignalStrength', 'id', 'my_s');
+    }
+
+    public function his_SignalStrength()
+    {
+        return $this->hasOne('App\SignalStrength', 'id', 'his_s');
+    }
+
+    public function my_Tone()
+    {
+        return $this->hasOne('App\Tone', 'id', 'my_t');
+    }
+
+    public function his_Tone()
+    {
+        return $this->hasOne('App\Tone', 'id', 'his_t');
+    }
 }
--- app/Mode.php
+++ app/Mode.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Band extends Model
+class Mode extends Model
 {
-    //
+    protected $fillable = ['name'];
 }
