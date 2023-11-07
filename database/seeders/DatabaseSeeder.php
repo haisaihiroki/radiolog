bc73a7a04e58aa1bf19d435cdda2477987d37e0a
Hiroki Chinen <haisaihiroki@gmail.com>
2023-11-08 00:36:19 +0900

    add DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             ReadabilitiesTableSeeder::class,
             SignalStrengthsTableSeeder::class,
             TonesTableSeeder::class,
+            BandsTableSeeder::class,
         ]);
     }
 }
