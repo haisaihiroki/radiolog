2c7daa90bf70a6781097dfc23508ee7c88c1d0f2
Hiroki Chinen <haisaihiroki@gmail.com>
2023-11-07 22:58:18 +0900

    bugfix

--- resources/views/layouts/app-summary.blade.php
+++ resources/views/layouts/app-summary.blade.php
@@ -57,7 +57,6 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('home') }}">Analog</a>
                                     <a class="dropdown-item" href="{{ route('home-digital') }}">Digital</a>
-                                    <a class="dropdown-item" href="{{ route('summary') }}">Summary</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
