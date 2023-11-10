2c3b7cd9233d71121f55298c3e575d9e9254d326
Hiroki Chinen <haisaihiroki@gmail.com>
2023-11-10 22:43:44 +0900

    add function

--- app/Band.php
+++ app/Band.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Band extends Model
 {
-    
+    public function getBand($freq)
+    {
+        $bands = Band::all();
+
+        foreach($bands as $val)
+        {
+            if ($val->lowwer_freq < $freq && $freq < $val->upper_freq)
+            {
+                return $val->band;
+            }
+        }
+        return NULL;
+    }
 }
