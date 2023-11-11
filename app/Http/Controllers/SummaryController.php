0265e82f72c626fdc5ef7dcc3e793def16653cab
Hiroki Chinen <haisaihiroki@gmail.com>
2023-11-12 00:15:17 +0900

    update

--- app/Http/Controllers/SummaryController.php
+++ app/Http/Controllers/SummaryController.php
@@ -9,6 +9,7 @@ use App\Mode;
 use App\Readability;
 use App\SignalStrength;
 use App\Tone;
+use App\Band;
 
 
 class SummaryController extends Controller
@@ -32,38 +33,19 @@ class SummaryController extends Controller
     {
         $user = Auth::user();
         
-        #band 1.8MHz-1.9MHz
-        $band_160 = $user->communicationLogs()->where('band', 'regexp', '^1\.[89]\d*')->get();
-        #band 3.5MHz-3.8MHz
-        $band_80 = $user->communicationLogs()->where('band', 'regexp','^3\.[5678]\d*')->get();
-        #band 7.0MHz-7.3MHz
-        $band_40 = $user->communicationLogs()->where('band', 'regexp','^7\.[012]\d*')->get();
-        #band[10.100, 10.150]
-        $band_30 = $user->communicationLogs()->where('band', 'regexp','10\.1[01234]\d*')->get();
-        #band[14.000, 14.350]
-        $band_20 = $user->communicationLogs()->where('band', 'regexp','14\.[123]\d*')->get();
-        #band[18.068, 18.168]
-        $band_17 = $user->communicationLogs()->where('band', 'regexp','18\.[01]\d*')->get();
-        #band[21.000, 21.450]
-        $band_15 = $user->communicationLogs()->where('band', 'regexp','21\.[01234]\d*')->get();
-        #band[24.890, 24.990]
-        $band_12 = $user->communicationLogs()->where('band', 'regexp','24\.[89]\d*')->get();
-        #band[28.000, 29.700]
-        $band_10 = $user->communicationLogs()->where('band', 'regexp','2[89]\.\d*')->get();
-        #band[50.000, 54.000]
-        $band_6 = $user->communicationLogs()->where('band', 'regexp','5[0123]\.\d*')->get();
-        #band[144.000, 146.000]
-        $band_2 = $user->communicationLogs()->where('band', 'regexp','14[45]\.\d*')->get();
-        #band[430.000, 440.000]
-        $band_430 = $user->communicationLogs()->where('band', 'regexp','43\d\.\d*')->get();
-        #band[1260.000, 1300.000]
-        $band_1200 = $user->communicationLogs()->where('band', 'regexp','12[6789]\d\.\d*')->get();
+        $summary = array();
+        $bands = Band::all();
+        foreach ($bands as $band)
+        {
+            $tmp = $user->communicationLogs()->where('band_id', $band->id)->get();
+            array_push($summary, $tmp);
+        }
 
         $total = $user->communicationLogs()->get();
         $mode_analogs = Mode::getAnalogList();
         $mode_digitals = Mode::getDigitalList();
 
-        return view('summary', compact('band_160', 'band_80', 'band_40', 'band_30', 'band_20', 'band_17', 'band_15', 'band_12', 'band_10', 'band_6', 'band_2', 'band_430', 'band_1200', 'total', 'mode_analogs', 'mode_digitals'));
+        return view('summary', compact('bands', 'summary', 'total', 'mode_analogs', 'mode_digitals'));
     }
 
 }
--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -13,75 +13,35 @@
                     <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">160m</th>
-                        <th scope="col">80m</th>
-                        <th scope="col">40m</th>
-                        <th scope="col">30m</th>
-                        <th scope="col">20m</th>
-                        <th scope="col">17m</th>
-                        <th scope="col">15m</th>
-                        <th scope="col">12m</th>
-                        <th scope="col">10m</th>
-                        <th scope="col">6m</th>
-                        <th scope="col">2m</th>
-                        <th scope="col">70cm</th>
-                        <th scope="col">23cm</th>
+                        @foreach($bands as $band)
+                        <th scope="col">{{ $band->band }}</th>
+                        @endforeach
                         <th scope="col">Total</th>
                     </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td>Total</td>
-                            <td><x-zerogray :count="count($band_160)"/></td>
-                            <td><x-zerogray :count="count($band_80)"/></td>
-                            <td><x-zerogray :count="count($band_40)"/></td>
-                            <td><x-zerogray :count="count($band_30)"/></td>
-                            <td><x-zerogray :count="count($band_20)"/></td>
-                            <td><x-zerogray :count="count($band_17)"/></td>
-                            <td><x-zerogray :count="count($band_15)"/></td>
-                            <td><x-zerogray :count="count($band_12)"/></td>
-                            <td><x-zerogray :count="count($band_10)"/></td>
-                            <td><x-zerogray :count="count($band_6)"/></td>
-                            <td><x-zerogray :count="count($band_2)"/></td>
-                            <td><x-zerogray :count="count($band_430)"/></td>
-                            <td><x-zerogray :count="count($band_1200)"/></td>
+                            @foreach($summary as $val)
+                            <td><x-zerogray :count="count($val)"/></td>
+                            @endforeach
                             <td><x-zerogray :count="count($total)"/></td>
                         </tr>
                         @foreach($mode_analogs as $analog)
                         <tr>
                             <td>{{ $analog->name }}</td>
-                            <td><x-zerogray :count="count($band_160->where('mode_id', $analog->id))"/></td>
-                            <td><x-zerogray :count="count($band_80->where('mode_id', $analog->id))"/></td>
-                            <td><x-zerogray :count="count($band_40->where('mode_id', $analog->id))"/></td>
-                            <td><x-zerogray :count="count($band_30->where('mode_id', $analog->id))"/></td>
-                            <td><x-zerogray :count="count($band_20->where('mode_id', $analog->id))"/></td>
-                            <td><x-zerogray :count="count($band_17->where('mode_id', $analog->id))"/></td>
-                            <td><x-zerogray :count="count($band_15->where('mode_id', $analog->id))"/></td>
-                            <td><x-zerogray :count="count($band_12->where('mode_id', $analog->id))"/></td>
-                            <td><x-zerogray :count="count($band_10->where('mode_id', $analog->id))"/></td>
-                            <td><x-zerogray :count="count($band_6->where('mode_id', $analog->id))"/></td>
-                            <td><x-zerogray :count="count($band_2->where('mode_id', $analog->id))"/></td>
-                            <td><x-zerogray :count="count($band_430->where('mode_id', $analog->id))"/></td>
-                            <td><x-zerogray :count="count($band_1200->where('mode_id', $analog->id))"/></td>
+                            @foreach($summary as $val)
+                            <td><x-zerogray :count="count($val->where('mode_id', $analog->id))"/></td>
+                            @endforeach
                             <td><x-zerogray :count="count($total->where('mode_id', $analog->id))"/></td>
                         </tr>
                         @endforeach
                         @foreach($mode_digitals as $digital)
                         <tr>
                             <td>{{ $digital->name }}</td>
-                            <td><x-zerogray :count="count($band_160->where('mode_id', $digital->id))"/></td>
-                            <td><x-zerogray :count="count($band_80->where('mode_id', $digital->id))"/></td>
-                            <td><x-zerogray :count="count($band_40->where('mode_id', $digital->id))"/></td>
-                            <td><x-zerogray :count="count($band_30->where('mode_id', $digital->id))"/></td>
-                            <td><x-zerogray :count="count($band_20->where('mode_id', $digital->id))"/></td>
-                            <td><x-zerogray :count="count($band_17->where('mode_id', $digital->id))"/></td>
-                            <td><x-zerogray :count="count($band_15->where('mode_id', $digital->id))"/></td>
-                            <td><x-zerogray :count="count($band_12->where('mode_id', $digital->id))"/></td>
-                            <td><x-zerogray :count="count($band_10->where('mode_id', $digital->id))"/></td>
-                            <td><x-zerogray :count="count($band_6->where('mode_id', $digital->id))"/></td>
-                            <td><x-zerogray :count="count($band_2->where('mode_id', $digital->id))"/></td>
-                            <td><x-zerogray :count="count($band_430->where('mode_id', $digital->id))"/></td>
-                            <td><x-zerogray :count="count($band_1200->where('mode_id', $digital->id))"/></td>
+                            @foreach($summary as $val)
+                            <td><x-zerogray :count="count($val->where('mode_id', $digital->id))"/></td>
+                            @endforeach
                             <td><x-zerogray :count="count($total->where('mode_id', $digital->id))"/></td>
                         </tr>
                         @endforeach
