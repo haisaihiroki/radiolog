24432cc19014160f03802387fe51be2d3f06f242
Hiroki Chinen <haisaihiroki@gmail.com>
2025-06-18 15:32:50 +0000

    bootstrap5対応

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,11 +43,11 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('home-digital') }}">Digital</a>
                                     <a class="dropdown-item" href="{{ route('summary') }}">Summary</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
